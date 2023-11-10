401fdda99597361b8b9eedc53de695a32dd485ce
fajri-blin <fajriindonesia1945@gmail.com>
2023-11-10 09:38:18 +0700

    create new transaction DTO

--- API/DTOs/TransactionsDTO/NewTransactionDTO.cs
+++ API/DTOs/TransactionsDTO/NewTransactionDTO.cs
@@ -1,7 +1,22 @@
-﻿namespace API.DTOs.TransactionsDTO;
+﻿using API.Model.Entities;
+using System.Data.Common;
+
+namespace API.DTOs.TransactionsDTO;
 
 public class NewTransactionDTO
 {
-    public Guid EmployeeGuid { get; set; }
+    public Guid? EmployeeGuid { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public decimal TotalAmmount { get; set; }
 
+    public static explicit operator Transaction(NewTransactionDTO newTransactionDTO)
+    {
+        return new Transaction
+        {
+            Guid = Guid.NewGuid(),
+            EmployeeGuid = newTransactionDTO.EmployeeGuid,
+            TransactionsDate = newTransactionDTO.TransactionDate,
+            TotalAmmount = newTransactionDTO.TotalAmmount,
+        };
+    }
 }
