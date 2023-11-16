79c8c904046f8c385b4182202d880a270d556bbc
fajri-blin <fajriindonesia1945@gmail.com>
2023-11-16 13:28:18 +0700

    adding GetAll for Transactions (In Progress)

---
+++ API/Controllers/TransactionController.cs
@@ -0,0 +1,15 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+public class TransactionController : ControllerBase
+{
+    private readonly TransactionService _transactionService;
+
+    public TransactionController(TransactionService transactionService)
+    {
+        _transactionService = transactionService;
+    }
+}
